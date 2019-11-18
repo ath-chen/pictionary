46215c2cc3034393af304e8ab0e365c302948ccb
Athena Chen <athenachenn@gmail.com>
2019-11-17 20:38:32 -0500

    cleaned up more console logs

--- client/routes.js
+++ client/routes.js
@@ -4,9 +4,7 @@ import {Learn, HomePage} from './components'
 
 //COMPONENT
 class Routes extends Component {
-  componentDidMount() {
-    // this.props.loadInitialData()
-  }
+  componentDidMount() {}
 
   render() {
     return (
@@ -19,46 +17,3 @@ class Routes extends Component {
 }
 
 export default Routes
-
-//CONTAINER
-
-// const mapDispatch = dispatch => {
-//   return {
-//     loadInitialData() {
-//       dispatch(me())
-//     }
-//   }
-// }
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-// export default withRouter(connect(null, mapDispatch)(Routes))
-
-/**
- * PROP TYPES
- */
-// Routes.propTypes = {
-// loadInitialData: PropTypes.func.isRequired,
-// isLoggedIn: PropTypes.bool.isRequired
-// }
-
-// Routes placed here are available to all visitors
-// Routes placed here are available to all visitors
-//         <Route path="/login" component={Login} />
-//         <Route path="/signup" component={Signup} />
-//         {isLoggedIn && (
-//           <Switch>
-//             {/* Routes placed here are only available after logging in */}
-//             <Route path="/home" component={UserHome} />
-//           </Switch>
-//         )}
-//         {/* Displays our Login component as a fallback */}
-//         <Route component={Login} />
-
-// const mapState = state => {
-//   return {
-//     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-//     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-//     isLoggedIn: !!state.user.id
-//   }
-// }
